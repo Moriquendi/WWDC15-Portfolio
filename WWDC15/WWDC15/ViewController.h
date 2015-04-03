d407ec33ec55544da4da59181d29d5c4f7510fd0
Michal Smialko <michal.smialko@gmail.com>
2015-04-03 22:01:44 +0200

    Add empty Xcode project.

---
+++ WWDC15/WWDC15/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WWDC15
+//
+//  Created by Michal Smialko on 03/04/15.
+//  Copyright (c) 2015 Michal Smialko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WWDC15/WWDC15Tests/WWDC15Tests.m
@@ -0,0 +1,40 @@
+//
+//  WWDC15Tests.m
+//  WWDC15Tests
+//
+//  Created by Michal Smialko on 03/04/15.
+//  Copyright (c) 2015 Michal Smialko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface WWDC15Tests : XCTestCase
+
+@end
+
+@implementation WWDC15Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
