d6e39f5c86595c395beabc0cb7aaca973b2188c3
Michal Smialko <michal.smialko@gmail.com>
2015-04-22 11:32:59 +0200

    Add some 'Into the wild' views.

---
+++ WWDC15/WWDC15/Code/Controllers/IntoTheWildViewController.swift
@@ -0,0 +1,20 @@
+//
+//  IntoTheWildViewController.swift
+//  WWDC15
+//
+//  Created by Michal Smialko on 21/04/15.
+//  Copyright (c) 2015 Michal Smialko. All rights reserved.
+//
+
+import UIKit
+
+class IntoTheWildViewController: UIViewController {
+
+    // MARK: UIViewController
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
